dbaa47879d1731ecfe2c9e4f6fd33c75a839b2a6
TimoteoGambella <91702810+TimoteoGambella@users.noreply.github.com>
2023-01-27 19:29:19 -0300

    navbar

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,12 +1,17 @@
+import { useNavigate } from "react-router-dom"
+import logo1 from "../assets/logo1.svg"
+import menu from "../assets/menu.svg"
 
 const NavBar = () => {
-        return(
-            <>
-                <div className="navbar-container-mobile">
-
-                </div>
-            </>
-        )
-    }
+    const navigate=useNavigate()
+    return(
+        <>
+            <div className="navbar-container-mobile">
+                <img src={logo1} alt="LOGO" onClick={()=>navigate("/")}/>
+                <img src={menu} className="menu" id="menu" alt="MENU" />
+            </div>
+        </>
+    )
+}
     
 export default NavBar
