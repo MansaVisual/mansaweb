3be100a1bff81ad99e43f05cc208424de2a10384
TimoteoGambella <timi.gambella@hotmail.com>
2023-01-28 11:48:53 -0300

    banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,7 +1,15 @@
-import banner from "../assets/portada 1.svg"
 
 export default function Banner(){
     return(
-        <img src={banner} alt="BANNER" className="banner"/>
+        <div className="banner-container">
+            <div className="title">
+                <h1>Your Allies</h1>
+                <h1>To Give Flight</h1>
+                <h1>Your Business</h1>
+            </div>
+            <p className="subtititle">
+                At Mansa Visual we work to provide you the design and marketing solutions that your business needs.
+            </p>
+        </div>
     )
 }
