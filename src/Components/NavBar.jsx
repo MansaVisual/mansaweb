f21e896938461a99a4791a57a9a2558f90bc04f1
TimoteoGambella <91702810+TimoteoGambella@users.noreply.github.com>
2023-01-27 21:28:28 -0300

    navBar advance

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,14 +1,27 @@
+import { useState } from "react"
 import { useNavigate } from "react-router-dom"
 import logo1 from "../assets/logo1.svg"
-import menu from "../assets/menu.svg"
+import logo3 from "../assets/logo3.svg"
 
 const NavBar = () => {
     const navigate=useNavigate()
+
+    const [menuOpen,setMenuOpen]=useState(false)
+
     return(
         <>
-            <div className="navbar-container-mobile">
-                <img src={logo1} alt="LOGO" onClick={()=>navigate("/")}/>
-                <img src={menu} className="menu" id="menu" alt="MENU" />
+            <div className={`navbar-container-mobile ${menuOpen && "menuOpen"}`}>
+                <div style={{backgroundImage:menuOpen?`url(${logo3}`:`url(${logo1}`}} alt="LOGO" onClick={()=>navigate("/")} className={`${menuOpen?"logo-menu-open":"logo-menu-close"}`}/>
+                <button class={`hamburger hamburger--stack ${menuOpen&&"active"}`} type="button" onClick={()=>setMenuOpen(!menuOpen)}>
+                    <div class="inner">
+                        <span class="bar"></span>
+                        <span class="bar"></span> 
+                        <span class="bar"></span>
+                    </div>
+                </button>
+            </div>
+            <div className={`${menuOpen?"menu-open":"menu-close"}`}>
+
             </div>
         </>
     )
