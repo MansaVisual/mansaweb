d452744634260f8d6fcdbbcb036602073643512a
TimoteoGambella <91702810+TimoteoGambella@users.noreply.github.com>
2023-01-28 16:03:48 -0300

    .

---
+++ src/Components/Banner.jsx
@@ -0,0 +1,15 @@
+
+export default function Banner(){
+    return(
+        <div className="banner-container">
+            <div className="title">
+                <h1>Your Allies</h1>
+                <h1>To Give Flight</h1>
+                <h1>Your Business</h1>
+            </div>
+            <p className="subtititle">
+                At Mansa Visual we work to provide you the design and marketing solutions that your business needs.
+            </p>
+        </div>
+    )
+}
