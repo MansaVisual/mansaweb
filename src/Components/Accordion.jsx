232127a34627c7ffedde98ed1f7eafbec6fcccc6
TimoteoGambella <timi.gambella@hotmail.com>
2023-01-30 01:08:44 -0300

    accordion

--- src/Components/Accordion.jsx
+++ src/Components/Accordion.jsx
@@ -4,23 +4,63 @@ import add from "../assets/add.svg"
 export default function Accordion(){
 
     const arrayAccordion=[
-        "UX/UI Design",
-        "Web Development",
-        "App Development",
-        "Branding",
-        "Data, Analytics",
-        "Marketing & Promotion"
+        {
+            title:"UX/UI Design",
+            array:["Brand Strategy 1", "Brand, sdfsd 1", "Load generatoru 1"]        
+        },
+        {
+            title:"Web Development",
+            array:["Brand Strategy", "Brand, sdfsd", "Load generatoru"]        
+        },
+        {
+            title:"App Development",
+            array:["Brand Strategy", "Brand, sdfsd", "Load generatoru"]        
+        },
+        {
+            title:"Branding",
+            array:["Brand Strategy", "Brand, sdfsd", "Load generatoru"]        
+        },
+        {
+            title:"Data, Analytics",
+            array:["Brand Strategy", "Brand, sdfsd", "Load generatoru"]        
+        },
+        {
+            title:"Marketing & Promotion",
+            array:["Brand Strategy", "Brand, sdfsd", "Load generatoru"]        
+        }
     ]
 
     const [objOpen,setObjOpen]=useState("")
+
+    const onClickAccordion=(idParam)=>{
+        if(objOpen!==""){
+            document.getElementById(`accordion${objOpen}`).classList.add("accordion-close")
+            document.getElementById(`accordion${objOpen}`).classList.remove("accordion-open")
+        }
+        if(idParam!==objOpen){
+            setObjOpen(idParam)
+            document.getElementById(`accordion${idParam}`).classList.remove("accordion-close")
+            document.getElementById(`accordion${idParam}`).classList.add("accordion-open")
+        }
+    }
 
     return(
         <div className="accordion-container">
             {arrayAccordion.map((obj,i)=>{
                 return(
-                    <div className="card-accordion">
-                        <h2>{obj}</h2>
-                        <img src={add} alt="ADD" />
+                    <div key={i}>
+                        {i!==0&&<div className="line-accordion"></div>}
+                        <div className="card-accordion" onClick={()=>onClickAccordion(i)}>
+                            <h2>{obj.title}</h2>
+                            <img src={add} alt="ADD"/>
+                        </div>
+                        <div id={`accordion${i}`} className={`accordion${i} accordion-close`}>
+                            {obj.array.map((obj,ii)=>{
+                                return(
+                                    <h3 key={ii}>{obj}</h3>
+                                )
+                            })}
+                        </div>
                     </div>
                 )
             })}
