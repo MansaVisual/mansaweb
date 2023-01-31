051a6fdf68b10dd4a6a0554cac49c7cf16a047c4
TimoteoGambella <timi.gambella@hotmail.com>
2023-01-31 11:02:46 -0300

    accordionLogo

--- src/Components/Accordion.jsx
+++ src/Components/Accordion.jsx
@@ -32,7 +32,7 @@ export default function Accordion(){
 
     const [objOpen,setObjOpen]=useState("")
 
-    const onClickAccordion=(idParam)=>{
+    const onClickAccordion=async(idParam)=>{
         if(objOpen!==""){
             if(idParam===objOpen){
                 if(document.getElementById(`accordion${idParam}`).classList.value.indexOf("accordion-close")!==-1){
@@ -52,19 +52,57 @@ export default function Accordion(){
             document.getElementById(`accordion${idParam}`).classList.remove("accordion-close")
             document.getElementById(`accordion${idParam}`).classList.add("accordion-open")
         }
+
+        // -------------------------------------------------------------------------------------------------------------
+
         if(objOpen===""){
-            document.getElementById(`logo${idParam}`).style.display="none"
+            document.getElementById(`line${idParam}-1`).classList.add("line1-open")
+            document.getElementById(`line${idParam}-2`).classList.add("line2-open")
+    
+            setTimeout(() => {
+                document.getElementById(`line${idParam}-1`).style.display="none"
+            }, 200);
         }else if(objOpen===idParam){
-            if(document.getElementsByClassName("accordion-open").length!==0){
-                document.getElementById(`logo${objOpen}`).style.display="none"
+            if(document.getElementsByClassName("line2-open").length!==0){
+                document.getElementById(`line${objOpen}-1`).classList.add("line1-close")
+                document.getElementById(`line${objOpen}-2`).classList.add("line2-close")
+                document.getElementById(`line${objOpen}-1`).classList.remove("line1-open")
+                document.getElementById(`line${objOpen}-2`).classList.remove("line2-open")
+                
+                setTimeout(() => {
+                    document.getElementById(`line${objOpen}-1`).style.display="block"
+                }, 200);
             }else{
-                document.getElementById(`logo${objOpen}`).style.display="block"
+                document.getElementById(`line${objOpen}-1`).classList.add("line1-open")
+                document.getElementById(`line${objOpen}-2`).classList.add("line2-open")
+                document.getElementById(`line${objOpen}-1`).classList.remove("line1-close")
+                document.getElementById(`line${objOpen}-2`).classList.remove("line2-close")
+        
+                setTimeout(() => {
+                    document.getElementById(`line${objOpen}-1`).style.display="none"
+                }, 200);
             }
         }else{
-            document.getElementById(`logo${objOpen}`).style.display="block"
-            document.getElementById(`logo${idParam}`).style.display="none"
+            document.getElementById(`line${objOpen}-1`).classList.add("line1-close")
+            document.getElementById(`line${objOpen}-2`).classList.add("line2-close")
+            document.getElementById(`line${objOpen}-1`).classList.remove("line1-open")
+            document.getElementById(`line${objOpen}-2`).classList.remove("line2-open")
+    
+            setTimeout(() => {
+                document.getElementById(`line${objOpen}-1`).style.display="block"
+            }, 200);
+            
+            document.getElementById(`line${idParam}-1`).classList.add("line1-open")
+            document.getElementById(`line${idParam}-2`).classList.add("line2-open")
+            document.getElementById(`line${objOpen}-1`).classList.remove("line1-close")
+            document.getElementById(`line${objOpen}-2`).classList.remove("line2-close")
+    
+            setTimeout(() => {
+                document.getElementById(`line${idParam}-1`).style.display="none"
+            }, 200);
         }
     }
+
 
     return(
         <div className="accordion-container">
@@ -74,7 +112,8 @@ export default function Accordion(){
                         {i!==0&&<div className="line-accordion"></div>}
                         <div className="card-accordion" onClick={()=>{onClickAccordion(i)}}>
                             <h2>{obj.title}</h2>
-                            <img src={add} alt="ADD" id={`logo${i}`}/>
+                            <div className="line1" id={`line${i}-1`}></div>
+                            <div className="line2" id={`line${i}-2`}></div>
                         </div>
                         <div id={`accordion${i}`} className={`accordion${i} accordion-close`}>
                             {obj.array.map((obj,ii)=>{
