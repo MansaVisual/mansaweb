b8b996841889bcdd34202ac0bf11028cce29677e
TimoteoGambella <91702810+TimoteoGambella@users.noreply.github.com>
2023-02-01 14:45:41 -0300

    navbar Details

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,21 +1,36 @@
-import { useState } from "react"
 import { useNavigate } from "react-router-dom"
 import logo1 from "../assets/logo1.svg"
+import logo2 from "../assets/logo2.svg"
 import logo3 from "../assets/logo3.svg"
 import subLogo1 from "../assets/linkedin.svg"
 import subLogo2 from "../assets/envelope.svg"
 import subLogo3 from "../assets/calendar.svg"
+import { useEffect, useState } from "react"
 
-export default function Navbar() {
+export default function Navbar({menuOpen,setMenuOpen}) {
     const navigate=useNavigate()
 
-    const [menuOpen,setMenuOpen]=useState(false)
+    const [menuBar,setMenuBar]=useState(false)
 
+    useEffect(() => {
+        if(window.location.pathname==="/Contact"){
+            setMenuBar(true)
+        }else{
+            setMenuBar(false)
+        }
+    }, []);
     
     return(
         <>
             <div className={`navbar-container-mobile ${menuOpen && "menuOpen"}`}>
-                <div style={{backgroundImage:menuOpen?`url(${logo3}`:`url(${logo1}`}} alt="LOGO" onClick={()=>navigate("/")} className={`${menuOpen?"logo-menu-open":"logo-menu-close"}`}/>
+                <div style={{backgroundImage:!menuOpen && window.location.pathname==="/Contact" ? `url(${logo2}` : menuOpen ? `url(${logo3}`:`url(${logo1}`}} alt="LOGO"
+                    onClick={()=>{
+                        if(window.location.pathname==="/Contact"){
+                            setMenuBar(false)
+                        }
+                        navigate("/")
+                    }}className={`${menuOpen?"logo-menu-open":"logo-menu-close"}`}
+                />
                 <button className={`hamburger hamburger--stack ${menuOpen&&"active"}`} type="button"
                     onClick={()=>{
                         window.scrollTo({
@@ -25,17 +40,39 @@ export default function Navbar() {
                         setMenuOpen(!menuOpen)
                     }}
                 >
-                    <div className="inner">
-                        <span className="bar"></span>
-                        <span className="bar"></span> 
-                        <span className="bar"></span>
+                    <div className="inner"
+                        onClick={()=>{
+                            if(window.location.pathname==="/Contact" && menuBar){
+                                setMenuBar(false)
+                            }else if(window.location.pathname==="/Contact" && !menuBar){
+                                setMenuBar(true)
+                            }
+                        }}
+                    >
+                        <span className={`bar ${menuBar&&"bar2"}`}></span>
+                        <span className={`bar ${menuBar&&"bar2"} ${menuBar&&"bar2-background"}`}></span>
+                        <span className={`bar ${menuBar&&"bar2"}`}></span>
                     </div>
                 </button>
             </div>
             <div className={`${menuOpen?"menu-open":"menu-close"}`}>
                 <div className="navbar-bar">
-                    <h4>Home</h4>
-                    <h4>Contact</h4>
+                    <h4 
+                        onClick={()=>{
+                            if(window.location.pathname==="/Contact"){
+                                setMenuBar(false)
+                                navigate("/")
+                            }
+                        }}
+                    >Home</h4>
+                    <h4 
+                        onClick={()=>{
+                            if(window.location.pathname==="/"){
+                                setMenuBar(true)
+                                navigate("/Contact")
+                            }
+                        }}
+                    >Contact</h4>
                     <h5>Offices</h5>
                 </div>
                 <div className="navbar-logos">
