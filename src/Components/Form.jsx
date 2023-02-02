a516ad1fcb311d70f554cdf2310f580150b6d589
TimoteoGambella <91702810+TimoteoGambella@users.noreply.github.com>
2023-02-02 11:59:07 -0300

    form

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,7 +1,23 @@
+import { TextField } from "@mui/material"
+import { useState } from "react"
 
 export default function Form(){
+    const [form,setForm]=useState({
+        mail:"",
+        name:"",
+        phone:"",
+        company:"",
+        description:""
+    })
+
     return(
         <div className="form-container">
+            <p className="title">Contact Us</p>
+            <p className="subtitle">Mail to <span>info@mansavisual</span> or book an appointment in the <span>www.app.com</span></p>
+
+            <div className="form">
+                <TextField id="standard-basic" label="Standard" variant="standard" />
+            </div>
         </div>
     )
 }
