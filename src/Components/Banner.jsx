fd886d25a716d73b829469d27dd81ab050372209
TimoteoGambella <91702810+TimoteoGambella@users.noreply.github.com>
2023-02-02 10:23:20 -0300

    banner

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -5,7 +5,7 @@ export default function Banner(){
             <div className="title">
                 <h1>Your Allies</h1>
                 <h1>To Give Flight</h1>
-                <h1>Your Business</h1>
+                <h1>To Your Business</h1>
             </div>
             <p className="subtititle">
                 At Mansa Visual we work to provide you the design and marketing solutions that your business needs.
