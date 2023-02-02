924a29294c1c571d19b8a709edafd1439cf4eabe
TimoteoGambella <91702810+TimoteoGambella@users.noreply.github.com>
2023-02-02 20:37:50 -0300

    form advance

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,7 +1,8 @@
 import { TextField } from "@mui/material"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 export default function Form(){
+
     const [form,setForm]=useState({
         mail:"",
         name:"",
@@ -9,6 +10,33 @@ export default function Form(){
         company:"",
         description:""
     })
+    
+    const arrayForm=[
+        {
+            title:"Mail",
+            form:"mail"
+        },
+        {
+            title:"Name",
+            form:"name"
+        },
+        {
+            title:"Phone",
+            form:"phone"
+        },
+        {
+            title:"Company Name",
+            form:"company"
+        },
+        {
+            title:"Describe Your Project",
+            form:"description"
+        },
+    ]
+
+    useEffect(() => {
+    console.log(form)
+    }, [form])
 
     return(
         <div className="form-container">
@@ -16,7 +44,17 @@ export default function Form(){
             <p className="subtitle">Mail to <span>info@mansavisual</span> or book an appointment in the <span>www.app.com</span></p>
 
             <div className="form">
-                <TextField id="standard-basic" label="Standard" variant="standard" />
+                {arrayForm.map((obj,i)=>{
+                    return(
+                        <TextField key={i} id={obj.title} variant="standard" label={obj.title} 
+                            onChange={(e)=>setForm({...form,[obj.form]:e.target.value})}
+                            multiline={true}
+                        />
+                    )
+                })}
+                <div className="button">
+                    <p>SEND</p>
+                </div>
             </div>
         </div>
     )
