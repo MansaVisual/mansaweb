b383436824a59f3f5ac38963d50b59a81265d908
TimoteoGambella <91702810+TimoteoGambella@users.noreply.github.com>
2023-02-03 11:15:32 -0300

    mouseDown

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -10,6 +10,7 @@ export default function Banner(){
             <p className="subtititle">
                 At Mansa Visual we work to provide you the design and marketing solutions that your business needs.
             </p>
+            <div className="mouse"></div>
         </div>
     )
 }
