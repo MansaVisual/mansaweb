2f1b936e36ac0c24ee4d1755ea66984c7ffa27c7
TimoteoGambella <91702810+TimoteoGambella@users.noreply.github.com>
2023-02-03 21:14:11 -0300

    banner-loading

--- src/App.js
+++ src/App.js
@@ -3,22 +3,53 @@ import Home from "./views/Home"
 import "./styles/styles.scss"
 import Navbar from "./Components/Navbar";
 import Contact from "./views/Contact";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 
 const App = () =>{
     const [menuOpen,setMenuOpen]=useState(false)
 
+    const [banner,setBanner]=useState(true)
+    const [blurNumber,setBlurNumber]=useState(0)
+
+    useEffect(()=>{
+        setTimeout(() => {
+            setBanner(false)
+        }, 1500);
+
+        
+        let load=0
+        const blurring = ()=>{
+
+            load++
+    
+            if (load > 99) {
+                clearInterval(int)
+                document.getElementById("bg").classList.add("banner-loading-close")
+            }
+
+            setBlurNumber(load)
+
+            document.getElementById("bg").style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
+        }
+
+        let int = setInterval(blurring,15)
+    },[])
+
+    
+    const scale = (num, in_min, in_max, out_min, out_max) => {
+        return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
+    }
+
     return(
-        <>
-            <Router>
-                <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
-                <Routes>
-                    <Route path="/" element={<Home menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>} />
-                    <Route path="/Contact" element={<Contact menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>} />
-                </Routes>
-            </Router>
-        </>
+        <Router>
+            {!banner&&<Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>}
+            <div className="banner-loading" id="bg"></div>
+            <Routes>
+                <Route path="/" element={<Home banner={banner} menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>} />
+                <Route path="/Contact" element={<Contact menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>} />
+            </Routes>
+        </Router>
     )
 } 
 
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -6,8 +6,8 @@ import Accordion from "../Components/Accordion"
 import Ourteam from "../Components/OurTeam"
 import { useEffect } from "react"
 
-const Home = ({menuOpen,setMenuOpen}) =>{
-
+const Home = ({menuOpen,setMenuOpen,banner}) =>{
+    
     useEffect(() => {
         if(menuOpen){
             setMenuOpen(!menuOpen)
@@ -18,47 +18,52 @@ const Home = ({menuOpen,setMenuOpen}) =>{
         });
     }, [])
 
+
     return(
-        <div className="home-container">
-            <Banner/>
-            
-            <div className="home-container-2">
-                <div className="first-box gradient-background" onClick={()=>window.open("https://calendly.com/mansavisual/15min")}>
-                    <h2>Web Development & Design Agency</h2>
-                    <p>We create experiences and build products all together. We make your business grow</p>
-                    <p className="link">Schedule a meeting here</p>
+        <>
+            {!banner &&
+                <div className="home-container">
+                    <Banner/>
+                    
+                    <div className="home-container-2">
+                        <div className="first-box gradient-background" onClick={()=>window.open("https://calendly.com/mansavisual/15min")}>
+                            <h2>Web Development & Design Agency</h2>
+                            <p>We create experiences and build products all together. We make your business grow</p>
+                            <p className="link">Schedule a meeting here</p>
+                        </div>
+                        
+                        <TitlesHome titleClass={"services"} title={"Services"}/>
+
+                        <Accordion/>
+
+                        <TitlesHome titleClass={"clients"} title={"Clients"}/>
+
+                        <span className="button-schedule gradient-background" onClick={()=>window.open("https://calendly.com/mansavisual/15min")}>
+                            <img src={calendar} alt="CALENDAR" />
+                            Schedule Meeting
+                        </span>
+
+                        <div className="ourTeam-eagle">
+                            <img src={eagle} alt="EAGLE" />
+                        </div>
+
+                        <TitlesHome titleClass={"team"} title={"Our Team"}/>
+                        <p className="ourTeam-description">
+                            We have a global team mainly focused in Argentina, Italy and the USA. This allows us to improve productivity as well as creativity and  service.
+                        </p>
+
+                        <Ourteam/>
+
+                    </div>
+                    <div className="home-footer gradient-background">
+                        <p>2023 Mansa Visual. All Rights Reserved.</p>
+                        <p>
+                            Terms Of Use. Privacy Policy
+                        </p>
+                    </div>
                 </div>
-                
-                <TitlesHome titleClass={"services"} title={"Services"}/>
-
-                <Accordion/>
-
-                <TitlesHome titleClass={"clients"} title={"Clients"}/>
-
-                <span className="button-schedule gradient-background" onClick={()=>window.open("https://calendly.com/mansavisual/15min")}>
-                    <img src={calendar} alt="CALENDAR" />
-                    Schedule Meeting
-                </span>
-
-                <div className="ourTeam-eagle">
-                    <img src={eagle} alt="EAGLE" />
-                </div>
-
-                <TitlesHome titleClass={"team"} title={"Our Team"}/>
-                <p className="ourTeam-description">
-                    We have a global team mainly focused in Argentina, Italy and the USA. This allows us to improve productivity as well as creativity and  service.
-                </p>
-
-                <Ourteam/>
-
-            </div>
-            <div className="home-footer gradient-background">
-                <p>2023 Mansa Visual. All Rights Reserved.</p>
-                <p>
-                    Terms Of Use. Privacy Policy
-                </p>
-            </div>
-        </div>
+            }
+        </>
     )
 }
 
