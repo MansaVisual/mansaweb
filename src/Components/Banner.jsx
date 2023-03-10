37f1411952a55362e214436d39cb79105b495fa3
TimoteoGambella <timi.gambella@hotmail.com>
2023-03-10 00:31:16 -0300

    banner

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,17 +1,20 @@
+import { useMediaQuery } from "@mui/material"
 
 export default function Banner(){
+    const isDesktop = useMediaQuery("(min-width:1024px")
+
     return(
-        <div className="banner-container">
+        <div className={`banner-container ${isDesktop?"desk":"mobile"}`}>
             <div className="title">
                 <h1>Your allies</h1>
                 <h1>to give flight</h1>
                 <h1>to your business</h1>
             </div>
-            <p className="subtititle">
+            <p className="subtitle">
                 At Mansa Visual we work toward your goals through the design and marketing solutions your business needs.
             </p>
             <div className="mouse-container">
-                <div className="mouse"></div>
+                <div className={`mouse ${isDesktop?"desk":"mobile"}`}></div>
             </div>
         </div>
     )
