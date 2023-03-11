ac4b01dc6a7f762bd51232acdff6bb091f0765b3
TimoteoGambella <91702810+TimoteoGambella@users.noreply.github.com>
2023-03-11 11:26:19 -0300

    forms

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,9 +1,8 @@
-import { TextField } from "@mui/material"
+import { useMediaQuery } from "@mui/material"
 import { Fragment, useEffect, useState } from "react"
 import emailjs from "@emailjs/browser";
-import tick from "../assets/form-tick.svg"
-import cross from "../assets/form-cross.svg"
-import eagle from "../assets/eagle-form.svg"
+import FormMobile from "./FormMobile";
+import FormDesk from "./FormDesk";
 
 export default function Form(){
 
@@ -119,86 +118,29 @@ export default function Form(){
         }, 1500);
     }
 
+    const isDesktop = useMediaQuery("(min-width:1024px")
+
     return(
-        <div className="form-container">
+        <div className={`form-container ${isDesktop?"desk":"mobile"}`}>
             {mailSend===null &&
                 <>
-                    <p className="title">Contact Us</p>
+                    {!isDesktop&&<p className="title">Contact Us</p>}
                     <p className="subtitle">To discuss a new project or find out more about us, do not hesitate to contact info@mansastudio.com or call our offices:</p>
                 </>
             }
-
-            <div className="form">
-                {mailSend===null &&
-                    <>
-                        {arrayForm.map((obj,i)=>{
-                            return(
-                                <div key={i}>
-                                    <TextField id={obj.title} variant="standard" label={`${obj.title}${!obj.optional?"*":""}`} 
-                                        onChange={(e)=>{
-                                            setForm({...form,[obj.form]:e.target.value})
-                                            if(obj.title==="Email"){
-                                                setErrorValidate(false)
-                                                setErrorMail(false)
-                                            }else if(obj.title==="Name"){
-                                                setErrorName(false)
-                                            }else if(obj.title==="Describe your project in short"){
-                                                setErrorDescription(false)
-                                            }
-                                        }}
-                                        multiline={true}
-                                        className={`
-                                            ${obj.title==="Email" && errorValidate ? "MuiInputBase-inputMultiline-error" : ""}
-                                            ${obj.title==="Email" && errorMail ? "MuiInputBase-inputMultiline-error" : ""}
-                                            ${obj.title==="Name" && errorName ? "MuiInputBase-inputMultiline-error" : ""}
-                                            ${obj.title==="Describe your project in short" && errorDescription ? "MuiInputBase-inputMultiline-error" : ""}
-                                        `}
-                                    />
-                                    {obj.title==="Email" && errorValidate && <span className="label-error">Incorrect email</span>}
-                                    {obj.title==="Email" && errorMail && <span className="label-error">Please enter your email</span>}
-                                    {obj.title==="Email" && !errorMail && !errorValidate && <div className="label-error"></div>}
-                                    {obj.title==="Name" && errorName && <span className="label-error">Please enter your name</span>}
-                                    {obj.title==="Name" && !errorName && <div className="label-error"></div>}
-                                    {obj.title==="Describe your project in short" && errorDescription && <span className="label-error">Please enter a project's description</span>}
-                                    {obj.title==="Describe your project in short" && !errorDescription && <div className="label-error"></div>}
-
-                                    {obj.title==="Phone" && <div className="label-error"></div>}
-                                    {obj.title==="Company Name" && <div className="label-error"></div>}
-                                </div>
-                            )
-                        })}
-                        <button className="button">
-                            {!clickButton&&<p onClick={()=>handleClick()}>Send</p>}
-                            {clickButton&&<div className="button-loader"></div>}
-                        </button>
-                        <p className="text-form">By submitting this form I consent to having Mansa Studio collect and process my personal details and agree with Privacy policy.</p>
-                    </>
-                }
-                {mailSend===false && 
-                    <div className="form-state">
-                        <img src={cross} alt="X" />
-                        <p className="problem">Oops... Something went wrong.</p>
-                        <p>Please <span className="try-again" onClick={()=>{
-                            setClickButton(false)
-                            setMailSend(null)
-                        }}>try again</span></p>
-                        <button className="button" onClick={()=>{
-                            setClickButton(false)
-                            setMailSend(null)
-                        }}>
-                            Try again
-                        </button>
-                    </div>
-                }
-                {mailSend===true && 
-                    <div className="form-state">
-                        <img src={tick} alt="X" />
-                        <p className="description-thank">Thank you!</p>
-                        <p className="description">We have received her message. We will try to respond in a few hours.</p>
-                        <img src={eagle} alt="EAGLE" className="eagle"/>
-                    </div>
-                }
-            </div>
+            {!isDesktop ?
+                <FormMobile 
+                    mailSend={mailSend} arrayForm={arrayForm} setForm={setForm} form={form} setErrorDescription={setErrorDescription} setErrorMail={setErrorMail}
+                    setErrorValidate={setErrorValidate} errorName={errorName} errorValidate={errorValidate} errorMail={errorMail} errorDescription={errorDescription}
+                    clickButton={clickButton} handleClick={handleClick} setClickButton={setClickButton} setMailSend={setMailSend}
+                />
+            :
+                <FormDesk 
+                    mailSend={mailSend} arrayForm={arrayForm} setForm={setForm} form={form} setErrorDescription={setErrorDescription} setErrorMail={setErrorMail}
+                    setErrorValidate={setErrorValidate} errorName={errorName} errorValidate={errorValidate} errorMail={errorMail} errorDescription={errorDescription}
+                    clickButton={clickButton} handleClick={handleClick} setClickButton={setClickButton} setMailSend={setMailSend}
+                />
+            }
         </div>
     )
 }
---
+++ src/Components/FormMobile.jsx
@@ -0,0 +1,80 @@
+import { TextField } from "@mui/material"
+import tick from "../assets/form-tick.svg"
+import cross from "../assets/form-cross.svg"
+import eagle from "../assets/eagle-form.svg"
+
+export default function FormMobile({mailSend,arrayForm,setForm,form,setErrorValidate,setErrorMail,setErrorName,setErrorDescription,errorName,errorValidate,errorDescription,errorMail,clickButton,handleClick,setClickButton,setMailSend}){
+    return(
+        <div className="form mobile">
+            {mailSend===null &&
+                <>
+                    {arrayForm.map((obj,i)=>{
+                        return(
+                            <div key={i}>
+                                <TextField id={obj.title} variant="standard" label={`${obj.title}${!obj.optional?"*":""}`} 
+                                    onChange={(e)=>{
+                                        setForm({...form,[obj.form]:e.target.value})
+                                        if(obj.title==="Email"){
+                                            setErrorValidate(false)
+                                            setErrorMail(false)
+                                        }else if(obj.title==="Name"){
+                                            setErrorName(false)
+                                        }else if(obj.title==="Describe your project in short"){
+                                            setErrorDescription(false)
+                                        }
+                                    }}
+                                    multiline={true}
+                                    className={`
+                                        ${obj.title==="Email" && errorValidate ? "MuiInputBase-inputMultiline-error" : ""}
+                                        ${obj.title==="Email" && errorMail ? "MuiInputBase-inputMultiline-error" : ""}
+                                        ${obj.title==="Name" && errorName ? "MuiInputBase-inputMultiline-error" : ""}
+                                        ${obj.title==="Describe your project in short" && errorDescription ? "MuiInputBase-inputMultiline-error" : ""}
+                                    `}
+                                />
+                                {obj.title==="Email" && errorValidate && <span className="label-error">Incorrect email</span>}
+                                {obj.title==="Email" && errorMail && <span className="label-error">Please enter your email</span>}
+                                {obj.title==="Email" && !errorMail && !errorValidate && <div className="label-error"></div>}
+                                {obj.title==="Name" && errorName && <span className="label-error">Please enter your name</span>}
+                                {obj.title==="Name" && !errorName && <div className="label-error"></div>}
+                                {obj.title==="Describe your project in short" && errorDescription && <span className="label-error">Please enter a project's description</span>}
+                                {obj.title==="Describe your project in short" && !errorDescription && <div className="label-error"></div>}
+
+                                {obj.title==="Phone" && <div className="label-error"></div>}
+                                {obj.title==="Company Name" && <div className="label-error"></div>}
+                            </div>
+                        )
+                    })}
+                    <button className="button">
+                        {!clickButton&&<p onClick={()=>handleClick()}>Send</p>}
+                        {clickButton&&<div className="button-loader"></div>}
+                    </button>
+                    <p className="text-form">By submitting this form I consent to having Mansa Studio collect and process my personal details and agree with Privacy policy.</p>
+                </>
+            }
+            {mailSend===false && 
+                <div className="form-state">
+                    <img src={cross} alt="X" />
+                    <p className="problem">Oops... Something went wrong.</p>
+                    <p>Please <span className="try-again" onClick={()=>{
+                        setClickButton(false)
+                        setMailSend(null)
+                    }}>try again</span></p>
+                    <button className="button" onClick={()=>{
+                        setClickButton(false)
+                        setMailSend(null)
+                    }}>
+                        Try again
+                    </button>
+                </div>
+            }
+            {mailSend===true && 
+                <div className="form-state">
+                    <img src={tick} alt="X" />
+                    <p className="description-thank">Thank you!</p>
+                    <p className="description">We have received her message. We will try to respond in a few hours.</p>
+                    <img src={eagle} alt="EAGLE" className="eagle"/>
+                </div>
+            }
+        </div>
+    )
+}
