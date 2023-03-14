fa193780f723e733053277c84475aa41bf47d5bf
TimoteoGambella <timi.gambella@hotmail.com>
2023-03-14 02:28:48 -0300

    carousel

--- src/Components/CarouselC.jsx
+++ src/Components/CarouselC.jsx
@@ -2,6 +2,15 @@ import Carousel from "react-multi-carousel";
 import arrowL from "../assets/carousel-l.svg"
 import arrowR from "../assets/carousel-r.svg"
 import "react-multi-carousel/lib/styles.css";
+import foto1 from "../assets/clients/baterbys.svg"
+import foto2 from "../assets/clients/famiq.svg"
+import foto3 from "../assets/clients/harebell.svg"
+import foto4 from "../assets/clients/mr.svg"
+import foto5 from "../assets/clients/patin.svg"
+import foto6 from "../assets/clients/remax.svg"
+import foto7 from "../assets/clients/swap-logo 1.svg"
+import foto8 from "../assets/clients/xirgu.svg"
+import { useMediaQuery } from "@mui/material";
 
 const responsive = {
     desktop: {
@@ -36,23 +45,27 @@ const PrevArrow = (props) => {
     );
   };
 
-  const x = [1,2,3,4,5,6,7]
+  const x = [foto1,foto2,foto3,foto4,foto5,foto6,foto7,foto8]
 
+  
 export default function CarouselC(){
+  const isDesktop = useMediaQuery("(min-width:1024px")
+
     return(
-        <div className="carousel-container">
-            <Carousel
-                responsive={responsive}
-                infinite={true}
-                customLeftArrow={<PrevArrow />}
-                customRightArrow={<NextArrow />}
-            >
-                {x.map((obj,i)=>{
-                    return(
-                        <div key={i}>{obj}</div>
-                    )
-                })}
-            </Carousel>
-        </div>
+      <div className={`carousel-container ${isDesktop&&"desk"}`}>
+          <Carousel
+              responsive={responsive}
+              infinite={true}
+              autoPlay={true}
+              customLeftArrow={<PrevArrow />}
+              customRightArrow={<NextArrow />}
+          >
+              {x.map((obj,i)=>{
+                  return(
+                    <img key={i} src={obj} alt="CLIENTES" />
+                  )
+              })}
+          </Carousel>
+      </div>
     )
 }
